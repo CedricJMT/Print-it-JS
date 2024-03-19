f42071929830fd5fcf6a0d3d274b83356495aed3
Cédric Jammet <cedricjmt@gmail.com>
2024-03-19 16:22:53 +0100

    ajout du javascript pour le carrousel, au complet. Fin de projet.

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,49 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+//-------------//
+
+
+
+//selection dans le DOM des éléments HTML//
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const bannerImg = document.querySelector(".banner-img");
+const tagLine = document.querySelector("#banner p");
+
+//index de l'image actuelle//
+let currentSlideIndex = 0;
+
+//fonctions//
+function showNextSlide() {
+    currentSlideIndex++;
+    if (currentSlideIndex >= slides.length) {
+        currentSlideIndex = 0;
+    }
+    updateSlide();
+}
+
+function showPreviousSlide() {
+    currentSlideIndex = currentSlideIndex - 1;
+    if (currentSlideIndex < 0) {
+        currentSlideIndex = slides.length - 1;
+    }
+    updateSlide();
+}
+
+function updateSlide(){
+	const slide = slides[currentSlideIndex]
+	bannerImg.src = `./assets/images/slideshow/${slide.image}`;
+	tagLine.innerHTML = slide.tagLine;
+}
+
+//comportement des fléches//
+arrowLeft.addEventListener("click", showPreviousSlide);
+arrowRight.addEventListener("click", showNextSlide);
+
+//1ere image au chargement page//
+updateSlide();
+
+
